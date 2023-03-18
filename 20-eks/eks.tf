c19f5c6aa47e6f00aa7006ed40c25f79448aa4c7
Deedeo <daradeo4u@gmail.com>
2023-03-18 00:42:53 +0000

    subnet update

--- 20-eks/eks.tf
+++ 20-eks/eks.tf
@@ -57,8 +57,11 @@ module "eks" {
   cluster_name    = local.environment_name
   cluster_version = "1.24"
 
+  
+  
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.public_subnets
+  
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
